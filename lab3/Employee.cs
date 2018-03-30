9e00457e9b9304b45bf8160b1c5d3e9d29e62440
Tanya Kraeva <t.a.kraevaa@gmail.com>
2018-03-30 16:54:51 +0300

    Исправлен класс Employee

--- lab3/Employee.cs
+++ lab3/Employee.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace lab3
 {
-    class Employee : Status
+    class Employee
     {
         public int ID_Employee
         { get; set; }
-        public string Position
+        public Position EmployeePosition
         { get; set; }
         public string Surname
         { get; set; }
@@ -23,10 +23,10 @@ namespace lab3
         public byte Experience
         { get; set; }
 
-        public Employee (int ID_Employee, string Position, string Surname, string Name, string Patronymic, int INN, byte Experience)
+        public Employee (int ID_Employee, Position EmployeePosition, string Surname, string Name, string Patronymic, int INN, byte Experience)
         {
             this.ID_Employee = ID_Employee;
-            this.Position = Position;
+            this.EmployeePosition = EmployeePosition;
             this.Surname = Surname;
             this.Name = Name;
             this.Patronymic = Patronymic;
@@ -37,11 +37,7 @@ namespace lab3
         {
             Console.WriteLine("Код сотрудника: {0}\nДолжность: {1}\nФамилия: {2}\nИмя: {3}\nОтчество: "
                 + "{4}\nИНН: {5}\nСтаж работы: {6}\n",
-                ID_Employee, Position, Surname, Name, Patronymic, INN, Experience);
-        }
-        void Status.BestEmployee(string Best)
-        {
-            Console.WriteLine("Лучший работник библиотеки: {0}", Best);
+                ID_Employee, EmployeePosition, Surname, Name, Patronymic, INN, Experience);
         }
     }
     
